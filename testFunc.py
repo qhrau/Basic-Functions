2e828912bd017a1d4744fb50f05995672158d913
DrSree <nilakant@iastate.edu>
2014-09-10 12:14:34 -0500

    Create testFunc.py

---
+++ testFunc.py
@@ -0,0 +1,12 @@
+""" define the following functions
+... getValue()
+... read an integer input from console and assign to local variable
+...
+... validateValue(value)
+... validate the value to be number
+...
+... findArea(var1, var2)
+... var1 and var2 are values collected from console and validated
+... return the product of var1 and var2 as area
+...
+...create the functions, save as modules, import the module and test.
